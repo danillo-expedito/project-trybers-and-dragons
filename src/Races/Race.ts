da9b52b8cc08fa7e447ea813bfdd686c6e27f093
Danillo Santos <dnashy@hotmail.com>
2023-09-22 16:20:01 -0300

    Conclui primeiro requisito, criando a classe abstrata Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,21 @@
+abstract class Race {
+  private static racesInstances = 0;
+
+  constructor(private _name: string, private _dexterity: number) {}
+
+  name(): string {
+    return this._name;
+  }
+
+  dexterity(): number {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+
+  static createdRacesInstances(): number | string {
+    throw new Error('Not implemented');
+  }
+}
+
+export default Race;
