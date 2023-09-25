bdb00b7ab5771fd28ac9fa59aa7be4ff6243e3b2
Danillo Santos <dnashy@hotmail.com>
2023-09-25 18:07:23 -0300

    Conclui décimo terceiro requisito, criando instancias de classes e função de batalha. Finaliza projeto

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('Ciri');
+const player2 = new Character('Yennefer');
+const player3 = new Character('Geralt');
+
+for (let i = 0, len = 5; i < len; i += 1) {
+  player1.levelUp();
+}
+// ------------------------------------------ //
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+// ------------------------------------------ //
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+// ------------------------------------------ //
+
+function runBattles(battles: Battle[]) {
+  for (let b = 0; b < battles.length; b += 1) {
+    const battle = battles[b];
+    battle.fight();
+  }
+}
+// ------------------------------------------ //
+
+export { 
+  player1, 
+  player2, 
+  player3, 
+  monster1, 
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
